64bca5a9cdac7c507dd169c405f66367afb53d1b
Diego1494 <91360430+Diego1494@users.noreply.github.com>
2021-12-08 14:19:44 -0600

    Error flow

---
+++ app/src/main/java/dev/cardoso/quotesmvvm/data/QuoteLocalDataSourceImpl.kt
@@ -0,0 +1,47 @@
+package dev.cardoso.quotesmvvm.data
+
+
+import dev.cardoso.quotesmvvm.core.toEntity
+import dev.cardoso.quotesmvvm.core.toListQuoteModel
+import dev.cardoso.quotesmvvm.core.toQuoteModel
+import dev.cardoso.quotesmvvm.data.local.QuoteLocalDataSource
+import dev.cardoso.quotesmvvm.data.local.daos.QuoteDAO
+import dev.cardoso.quotesmvvm.data.model.QuoteModel
+import dev.cardoso.quotesmvvm.data.model.QuoteResponse
+import dev.cardoso.quotesmvvm.data.remote.QuoteApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import org.json.JSONObject
+import org.json.JSONTokener
+import javax.inject.Inject
+
+
+class QuoteLocalDataSourceImpl  @Inject constructor(private val quoteDao: QuoteDAO): QuoteLocalDataSource {
+    override fun getQuotes(): Flow<List<QuoteModel>> {
+        val quotes = quoteDao.getQuotes()
+        return quotes.map { it.toListQuoteModel() }
+    }
+
+    override   fun getQuote(quoteId: Int): Flow<QuoteModel> {
+        return  quoteDao.getQuote(quoteId).map { it.toQuoteModel()}
+    }
+
+    override fun getQuoteRandom(): Flow<QuoteModel> {
+        return  quoteDao.getQuoteRandom().map { it.toQuoteModel() }
+    }
+
+    override suspend fun insertAll(quotes: List<QuoteModel>) {
+        quoteDao.insertAll(quotes!!.map { it.toEntity()})
+    }
+
+
+    override suspend fun insert(quoteModel: QuoteModel) {
+        quoteDao.insert(quoteModel.toEntity())
+    }
+
+}
+
+
+
+
